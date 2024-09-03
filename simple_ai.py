26a44a45de02ea26e196cdcc8611e4cb9e29db53
Lomholy <daniel.lomholt.2000@gmail.com>
2024-09-03 19:52:34 +0200

    Final version

--- simple_ai.py
+++ simple_ai.py
@@ -7,16 +7,11 @@ from supremacy import helpers
 # This is your team name
 CREATOR = "Quantum_supremacy"
 
-
 def tank_ai(tank, info, game_map):
     """
     Function to control tanks.
     """
-    if not tank.stopped:
-        if tank.stuck:
-            tank.set_heading(np.random.random() * 360.0)
-        elif "target" in info:
-            tank.goto(*info["target"])
+    tank.stop()
 
 
 def ship_ai(ship, info, game_map):
@@ -25,10 +20,10 @@ def ship_ai(ship, info, game_map):
     """
     if not ship.stopped:
         if ship.stuck:
-            if ship.get_distance(ship.owner.x, ship.owner.y) > 20:
+            if ship.get_distance(ship.owner.x, ship.owner.y) > 40:
                 ship.convert_to_base()
             else:
-                ship.set_heading(np.random.random() * 360.0)
+                ship.set_heading(ship.heading + np.random.random() * 360.0)
 
 
 def jet_ai(jet, info, game_map):
@@ -37,25 +32,19 @@ def jet_ai(jet, info, game_map):
     """
     if "target" in info:
         jet.goto(*info["target"])
+    else:
+        jet.set_heading(jet.heading + 1.5 * np.random.random())
 
 
 class PlayerAi:
     """
     This is the AI bot that will be instantiated for the competition.
     """
-    def starting_build_queue(self):
-        return helpers.BuildQueue(
-            ["mine", "ship", "mine", "tank","ship", "mine", "tank","jet"], cycle=False
-        )
-    def final_build_queue(self):
-        return helpers.BuildQueue(
-            ["ship", "jet"], cycle=True
-        )
+
     def __init__(self):
         self.team = CREATOR  # Mandatory attribute
-        self.build_queue = helpers.BuildQueue(
-            ["mine", "ship", "mine", "tank","ship", "tank","jet"], cycle=True
-        )
+        self.ntanks = {}
+        self.nships = {}
 
     def run(self, t: float, dt: float, info: dict, game_map: np.ndarray):
         """
@@ -67,19 +56,40 @@ class PlayerAi:
 
         # Iterate through all my bases and process build queue
         for base in myinfo["bases"]:
-            # Calling the build_queue will return the object that was built by the base.
-            # It will return None if the base did not have enough resources to build.
-            obj = self.build_queue(base)
+            # Bookkeeping
+            uid = base.uid
+            if uid not in self.ntanks:
+                self.ntanks[uid] = 0
+                self.nships[uid] = 0
+
+            # Firstly, each base should build a mine if it has less than 3 mines
+            if base.mines < 3:
+                if base.crystal > base.cost("mine"):
+                    base.build_mine()
+            # If we have enough mines, pick something at random
+            else:
+                variable = np.random.random_integers(0, 1)
+                if self.nships[uid] < 5:
+                    if base.crystal > base.cost("ship"):
+                        ship = base.build_ship(heading=360*np.random.random())
+                        self.nships[uid] += 1
+                elif self.ntanks[uid] < 3:
+                    if base.crystal > base.cost("tank"):
+                        tank = base.build_tank(heading=360*np.random.random())
+                        self.ntanks[uid] += 1
+                elif base.crystal > base.cost("jet"):
+                    jet = base.build_jet(heading=360*np.random.random())
 
         # Try to find an enemy target
         # If there are multiple teams in the info, find the first team that is not mine
+        targets=[]
         if len(info) > 1:
             for name in info:
                 if name != self.team:
-                    # Target only bases
-                    if "bases" in info[name]:
-                        # Simply target the first base
-                        t = info[name]["bases"][0]
+                    # Target ships & bases
+                    if  "bases" in info[name]:
+                        target = 0
+                        t = info[name]["bases"][target]
                         myinfo["target"] = [t.x, t.y]
                         break
 
@@ -87,3 +97,9 @@ class PlayerAi:
         helpers.control_vehicles(
             info=myinfo, game_map=game_map, tank=tank_ai, ship=ship_ai, jet=jet_ai
         )
+    def get_closest_base(self, bases, x, y):
+        closest = None
+        for base in bases:
+            if closest is None or base.get_distance(x, y) < closest.get_distance(x, y):
+                closest = base
+        return closest
